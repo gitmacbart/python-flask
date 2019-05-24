ed7f1b539c33b10b5474ede9450e79f69d13c486
Arnaud <gitmacbart@users.noreply.github.com>
2019-05-24 13:09:20 +0100

    Update index.py

--- about/index.py
+++ about/index.py
@@ -1,6 +1,6 @@
 from flask import Flask, Response, __version__
 app = Flask(__name__)
-source = 'https://github.com/zeit/now-examples/tree/master/python-flask'
+source = 'https://github.com/gitmacbart/python-flask.git'
 css = '<link rel="stylesheet" href="/css/style.css" />'
 
 @app.route('/', defaults={'path': ''})
@@ -8,4 +8,4 @@ css = '<link rel="stylesheet" href="/css/style.css" />'
 def catch_all(path):
     name = path.upper()
     v = __version__
-    return Response("%s<h1>%s</h1><ul><li>WSGI Enabled</li><li>Flask version <em>%s</em></li></ul><p>Visit the <a href='./'>home</a> page or view the <a href='%s'>source code</a>.</p>" % (css, name, v, source), mimetype='text/html')
+    return Response("%s<h1>%s</h1><ul><li>WSGI Enabled</li><li>Arnaud Enabled</li><li>Flask version <em>%s</em></li></ul><p>Visit the <a href='./'>home</a> page or view the <a href='%s'>source code</a>.</p>" % (css, name, v, source), mimetype='text/html')
